d8d91365922b85059883b9959e782c6b4fac7a19
a.rajabi <a.rajabi@dpkharazmi.com>
2023-06-26 16:59:51 +0330

    Add project files.

---
+++ CompoundCalc/Calc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompoundCalc
+{
+    public class Calc
+    {
+        public static decimal CompoundCalc(decimal initialFund,float Interest,int Period)
+        {
+            var MonthlyFund = new decimal[Period + 1];
+            MonthlyFund[0] = initialFund;
+            for (int i = 1; i <= Period; i++)
+            {
+                MonthlyFund[i] = MonthlyFund[i - 1] + decimal.Parse((float.Parse(MonthlyFund[i - 1].ToString()) * Interest).ToString()); 
+            }
+            return MonthlyFund.Last();
+        }
+    }
+}
---
+++ CompoundCalc/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompoundCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            startProc:
+            Console.WriteLine("Enter Initial Fund");
+            var InitFund = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Monthly Interest Percentage");
+            var Interest = float.Parse((float.Parse(Console.ReadLine())/100F).ToString());
+            Console.WriteLine("Enter Period By Month");
+            var Period = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("==================================================");
+            Console.WriteLine($"Fund after {Period} month would be : {Calc.CompoundCalc(InitFund,Interest,Period)}");
+            Console.WriteLine("");
+            Console.WriteLine("do you want to continue? Y/N");
+            var decide = Console.ReadLine();
+            if(decide.ToLower() == "y")
+                goto startProc;
+            Console.ReadKey();
+        }
+    }
+}
